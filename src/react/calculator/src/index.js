f10fc2555edb27e74fec6e4f7ae3ad5acc25f160
Excess <amos.dinh.2013@gmail.com>
2023-01-05 11:53:29 +0000

    routing working

--- src/react/calculator/src/index.js
+++ src/react/calculator/src/index.js
@@ -4,8 +4,23 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+  </Routes>
+</BrowserRouter>
 );
+{/* <React.StrictMode>
+    <App />
+  </React.StrictMode> */}
