a97b3bc45e707490abaf5dcdf1a8ccab1ebe9f24
Matthias Fast <matthias.fast@mail.de>
2023-01-16 18:36:50 +0100

    width gefixt

--- src/frontend/src/profile.js
+++ src/frontend/src/profile.js
@@ -73,45 +73,47 @@ function Profile() {
     }
     
     return (
-        <div class="container">
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to={"/"+username}>Feed</Link>
-                    <Link class="navbar-brand" to={"/profile/"+username+"/"+username}>{username==profileusername?<b>Profile</b>: <span>Profile</span>}</Link>
-                    <form class="d-flex" role="search" >
-                        <input {...registerSearch("searchterm", { required: true })}  class="form-control me-2" type="text" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" onClick={handleSubmitSearch(handleSearch)}>Search</button>
-                    </form>
-                </div>
-            </nav>
-            <div class="card">
-             
-                <div class="card-body">
-                    <h5 class="card-title">@{profileusername} <button id='followBtn' onClick={handleSubmit(follow)} type="button" class={"btn "+(userFollowsBool.data? "btn-light":"btn-secondary")+" btn-lg"}
-                >{userFollowsBool.data? "unfollow":"follow"}</button></h5>
-                    <p class="card-text"></p>
-                </div>
-                <ul class="list-group list-group-flush">
+        <main id='ProfileMain'>
+            <div class="container">
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid">
+                        <Link class="navbar-brand" to={"/"+username}>Feed</Link>
+                        <Link class="navbar-brand" to={"/profile/"+username+"/"+username}>{username==profileusername?<b>Profile</b>: <span>Profile</span>}</Link>
+                        <form class="d-flex" role="search" >
+                            <input {...registerSearch("searchterm", { required: true })}  class="form-control me-2" type="text" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" onClick={handleSubmitSearch(handleSearch)}>Search</button>
+                        </form>
+                    </div>
+                </nav>
+                <div class="card">
                 
-                </ul>
-            
+                    <div class="card-body">
+                        <h5 class="card-title">@{profileusername} <button id='followBtn' onClick={handleSubmit(follow)} type="button" class={"btn "+(userFollowsBool.data? "btn-light":"btn-secondary")+" btn-lg"}
+                    >{userFollowsBool.data? "unfollow":"follow"}</button></h5>
+                        <p class="card-text"></p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                    
+                    </ul>
+                
+                </div>
+
+                <h3 class="text-start">{data.length} Post{ data.length==1? "":'s' }:</h3>
+
+                <div id='Cardcontainer' class="container">
+                {
+                    postCard(data,username)
+                }
+
+
+                </div>
+                <Link to={'/post/' + username}>
+                    <button id="post" type="button" class="btn btn-primary btn-lg"
+                    >Posten</button>
+
+                </Link>
             </div>
-
-            <h3 class="text-start">{data.length} Post{ data.length==1? "":'s' }:</h3>
-
-            <div id='Cardcontainer' class="container">
-            {
-                postCard(data,username)
-            }
-
-
-            </div>
-            <Link to={'/post/' + username}>
-                <button id="post" type="button" class="btn btn-primary btn-lg"
-                >Posten</button>
-
-            </Link>
-        </div>
+        </main>
     );
 }
 
